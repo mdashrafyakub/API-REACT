651b1bde7406cc03f626643fc7deff6a55b69c99
Mohamed Ashraf M Y <mdashrafyakub@gmail.com>
2022-03-10 07:04:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [statuscode, setStatuscode] = useState(false);
+  function apidata() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        if (response.status === 200) {
+          setStatuscode(true);
+
+          return response.json();
+        }
+      })
+      .then((json) => setData(json));
+  }
+  useEffect(() => {
+    apidata();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {statuscode ? (
+        <div>
+          <div class="alert">
+            <span class="closebtn" onclick={() => setStatuscode(false)}>
+              &times;
+            </span>
+            <strong>Sucess!</strong> API call is Done.
+          </div>
+          {data.length > 0 && <p>{data.length}</p>}
+          {data.length > 0 && data.map((data) => <p>{data.name}</p>)}
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
